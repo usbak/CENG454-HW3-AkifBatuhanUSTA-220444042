90c8f48f8583d2bdd23e5fc76c04f18c209cede8
usbak <usbak28sdal@gmail.com>
2026-05-10 22:01:29 +0300

    Implemented strategy-based weapon system

---
+++ Assets/Scripts/Enemies/Movement/StraightMovement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+
+
+public class StraightMovement : IMovementStrategy
+{
+    public void Move(Transform self, Transform target, float speed, float deltaTime)
+    {
+        if (target == null) return;
+        Vector3 dir = (target.position - self.position).normalized;
+        self.position += dir * speed * deltaTime;
+    }
+}
